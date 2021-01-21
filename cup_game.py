d038e315fdd9377f734429c0aafa19273641d5ee
mxw0042 <mxw0042@gmail.com>
2021-01-21 00:03:41 -0500

    cleaned up and updated

--- cup_game.py
+++ cup_game.py
@@ -1,10 +1,4 @@
 from tkinter import *
-import time
-import cv2  
-import numpy as np 
-import matplotlib.pyplot as plt
-from matplotlib.colors import hsv_to_rgb
-from PIL import ImageGrab
 
 #Window class for making different windows
 class Window:
@@ -99,77 +93,15 @@ class Window:
         self._drag_data["y"] = event.y
 
 class DragCircle:
-#Constructor
+    #Constructor
     def __init__(self, window, width=100, height=100, colour="red"):
         self.window = window
         tag = "circle-%d" % id(self)
         self.circle = self.window._create_circle(width, height, colour, tag)
 
 class DragCup:
-#Constructor
+    #Constructor
     def __init__(self, window, width=100, height=100):
         self.window = window
         tag = "cup-%d" % id(self)
         self.cup = self.window._create_cup(width, height)
-        
-
-
-# Threshold of green in HSV space 
-lower_green = np.array([50, 100, 100]) 
-upper_green = np.array([70, 255, 255]) 
-
-# Threshold of blue in HSV space 
-lower_blue = np.array([110,200,200])
-upper_blue = np.array([130,255,255])
-
-# Threshold of yellow in HSV space 
-lower_yellow = np.array([30, 90, 90]) 
-upper_yellow = np.array([40, 255, 255])
-
-#Make a window from my own class
-window = Tk()
-window.title("Drag & Drop")
-
-#Create an instance of the window class
-main_window = Window(window)
-
-#Create a circle object from the DragCircle class
-circle = DragCircle(main_window, 100, 100, "yellow")
-circle2 = DragCircle(main_window, 200, 200, "green")
-circle3 = DragCircle(main_window, 300, 300, "blue")
-cup= DragCup(main_window, 50, 200)
-
-#Start the animation loop
-def task():
-    x=window.winfo_rootx()+main_window.canvas.winfo_x()
-    y=window.winfo_rooty()+main_window.canvas.winfo_y()
-    x1=x+main_window.canvas.winfo_width()+170
-    y1=y+main_window.canvas.winfo_height()+150
-    
-    img=ImageGrab.grab((x,y,x1,y1)).save("test.png")
-    frame = cv2.imread("test.png")
-    # It converts the BGR color space of image to HSV color space 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
-
-    # preparing the mask to overlay 
-    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue) 
-    mask_green = cv2.inRange(hsv, lower_green, upper_green) 
-    mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow) 
-        
-    # The black region in the mask has the value of 0, 
-    # so when multiplied with original image removes all non-blue regions 
-    result = cv2.bitwise_or(mask_blue, mask_green) 
-    result = cv2.bitwise_or(result, mask_yellow) 
-    # cv2.imshow('frame', frame) 
-    # cv2.imshow('mask', mask_blue) 
-    # cv2.imshow('mask', mask_green) 
-    # cv2.imshow('mask', mask_yellow) 
-    cv2.imshow('result', result) 
-    window.after(500, task)  # reschedule event in 2 seconds
-    
-
-window.after(500, task)
-window.mainloop()
-
-# Closes all the frames 
-cv2.destroyAllWindows() 
---
+++ ekf.py
@@ -0,0 +1,48 @@
+import numpy as np 
+
+def ekf(m, x, filterstep, error_cov, count):
+
+    R = np.diag([.0001, .0001])
+    I = np.eye(4)
+    
+    # Project the state ahead
+    x[0] = x[0] + filterstep*x[3]*np.cos(x[2])
+    x[1] = x[1] + filterstep*x[3]*np.sin(x[2])
+    x[2] = (x[2]+ np.pi) % (2.0*np.pi) - np.pi
+    x[3] = x[3]
+
+    # Calculate the Jacobian of the Dynamic Matrix A
+    a13 = filterstep*x[3]*np.sin(x[2])
+    a14 = filterstep*np.cos(x[2])
+    a23 = filterstep*x[3]*np.cos(x[2])
+    a24 = filterstep*np.sin(x[2])
+    JA = np.array([[1.0, 0.0, a13, a14],
+                    [0.0, 1.0, a23, a24],
+                    [0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 1.0]])
+
+    #Process noise cov
+    Q = np.diag([0.03, 0.03, 0.03, 0.03]) 
+    
+    # Project the error covariance ahead
+    error_cov = np.matmul(np.matmul(JA, error_cov), JA.T) + Q
+
+    # Measurement Function
+    hx = np.array([x[0], x[1]])
+
+    if count%2==0: #every nth step pretend there is a measurement
+        JH = np.array([[1.0, 0.0, 0.0, 0.0],
+                        [0.0, 1.0, 0.0, 0.0]])
+    else: # every other step
+        JH = np.array([[0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0]])  
+
+    S = np.matmul(np.matmul(JH, error_cov), JH.T) + R
+    K = np.matmul(np.matmul(error_cov, JH.T), np.linalg.inv(S.astype('float64')))
+
+    Z = np.array(m)
+    y = Z - hx 
+    x = x + np.matmul(K, y)
+    # Update the error covariance
+    error_cov = np.matmul((I - np.matmul(K, JH)), error_cov)
+    return x, error_cov
