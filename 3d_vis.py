454681e5015150c132c279e7b804060bcdf43eec
mxw0042 <mxw0042@gmail.com>
2021-04-13 14:58:24 -0400

    added single vector vis

--- 3d_vis.py
+++ 3d_vis.py
@@ -1,10 +1,13 @@
 import csv
 import numpy as np
 from spatialmath import *
+from spatialmath.pose3d import * 
 from math import pi
 
 import matplotlib; matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
 
 filename = "tf.csv"
 
@@ -22,22 +25,51 @@ with open(filename, 'r') as csvfile:
     print("Total no. of rows: %d"%(csvreader.line_num))
   
 count=0
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-for row in rows[:100]:
-    count+=1
-    if '"obj-10"' in row:   # start with just visualizing one object
-        q=UnitQuaternion([float(row[15]),float(row[16]),float(row[17]),float(row[18])])
-        ax.scatter(float(row[11]), float(row[12]), float(row[13]))
-        p=SE3(float(row[11]), float(row[12]), float(row[13]))
-        T=p * q.SO3()
-        T.plot(frame='1', dims=[0.1391,0.1395,-0.00025,0.0000,0.8505,0.8530])
-        plt.pause(0.0001)
-        print(count)
-        pose.append(T)
-print("done")
+fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
+
+# def get_arrow(x, y, z, q):
+#     u = np.sin(2*theta)
+#     v = np.sin(3*theta)
+#     w = np.cos(3*theta)
+#     return x,y,z,u,v,w
+
+# ax.set_xlim(0, 1)
+# ax.set_ylim(-.5, .5)
+# ax.set_zlim(0.5, 1)
+ax.set_xlim(-1, 1)
+ax.set_ylim(-1, 1)
+ax.set_zlim(0, 1)
+
+quiver_10 = ax.quiver(0, 0, 0, 0, 0, 0)
+quiver_7 = ax.quiver(0, 0, 0, 0, 0, 0)
+quiver_3 = ax.quiver(0, 0, 0, 0, 0, 0)
+quiver_4 = ax.quiver(0, 0, 0, 0, 0, 0)
+quiver_12 = ax.quiver(0, 0, 0, 0, 0, 0)
+
+def update(count):
+    global quiver_10, quiver_7, quiver_3, quiver_4, quiver_12
+    row=rows[count]
+    print(count)
+    if '"obj-10"' in row:
+        quiver_10.remove()   
+        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
+        quiver_10 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+    elif '"obj-7"' in row:
+        quiver_7.remove()
+        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
+        quiver_7 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+    elif '"obj-3"' in row:
+        quiver_3.remove()
+        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
+        quiver_3 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+    elif '"obj-4"' in row:
+        quiver_4.remove()
+        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
+        quiver_4 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+    elif '"obj-12"' in row:
+        quiver_12.remove()
+        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
+        quiver_12 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+
+ani = FuncAnimation(fig, update, frames=range(1500, len(rows)), interval=10, repeat=False)
 plt.show()
-
-
-
-# tranimate(transl(4, 3, 4)@trotx(2)@troty(-2), frame='2', arrow=False, dims=[0, 5], nframes=200)
