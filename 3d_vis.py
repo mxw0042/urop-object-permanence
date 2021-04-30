9e125d032d36fe56ff55ddbf53119c0408404d07
mxw0042 <mxw0042@gmail.com>
2021-04-30 15:34:11 -0400

    added multiple axes

--- 3d_vis.py
+++ 3d_vis.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 
-filename = "tf.csv"
+filename = "tf (2).csv"
 
 fields = []
 rows = []
@@ -36,40 +36,51 @@ fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
 # ax.set_xlim(0, 1)
 # ax.set_ylim(-.5, .5)
 # ax.set_zlim(0.5, 1)
-ax.set_xlim(-1, 1)
-ax.set_ylim(-1, 1)
-ax.set_zlim(0, 1)
+ax.set_xlim(-0.5, 0.5)
+ax.set_ylim(-0.5, 0.5)
+ax.set_zlim(0.5, 1)
 
-quiver_10 = ax.quiver(0, 0, 0, 0, 0, 0)
-quiver_7 = ax.quiver(0, 0, 0, 0, 0, 0)
-quiver_3 = ax.quiver(0, 0, 0, 0, 0, 0)
-quiver_4 = ax.quiver(0, 0, 0, 0, 0, 0)
-quiver_12 = ax.quiver(0, 0, 0, 0, 0, 0)
+axes_10=[ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0)]
+axes_7=[ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0)]
+axes_3=[ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0)]
+axes_4=[ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0)]
+axes_12=[ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0), ax.quiver(0, 0, 0, 0, 0, 0)]
+
+objs = {
+  '"obj-10"': axes_10,
+  '"obj-7"': axes_7,
+  '"obj-3"': axes_3,
+  '"obj-4"': axes_4,
+  '"obj-12"': axes_12
+}
+
+colors=['#E16F6D', '#4eb604', '#0d87c3']
 
 def update(count):
-    global quiver_10, quiver_7, quiver_3, quiver_4, quiver_12
+    global axes_10, axes_7, axes_3, axes_4, axes_12
     row=rows[count]
     print(count)
+    q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])]).R
     if '"obj-10"' in row:
-        quiver_10.remove()   
-        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
-        quiver_10 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+        for i in range(3):
+            axes_10[i].remove()   
+            axes_10[i] = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q[0][i], q[1][i], q[2][i], length=0.1, normalize=True, color=colors[i])
     elif '"obj-7"' in row:
-        quiver_7.remove()
-        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
-        quiver_7 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+        for i in range(3):
+            axes_7[i].remove()   
+            axes_7[i] = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q[0][i], q[1][i], q[2][i], length=0.1, normalize=True, color=colors[i])
     elif '"obj-3"' in row:
-        quiver_3.remove()
-        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
-        quiver_3 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+        for i in range(3):
+            axes_3[i].remove()   
+            axes_3[i] = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q[0][i], q[1][i], q[2][i], length=0.1, normalize=True, color=colors[i])
     elif '"obj-4"' in row:
-        quiver_4.remove()
-        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
-        quiver_4 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+        for i in range(3):
+            axes_4[i].remove()   
+            axes_4[i] = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q[0][i], q[1][i], q[2][i], length=0.1, normalize=True, color=colors[i])
     elif '"obj-12"' in row:
-        quiver_12.remove()
-        q=UnitQuaternion([float(row[18]), float(row[15]),float(row[16]),float(row[17])])
-        quiver_12 = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q.v[0], q.v[1], q.v[2])
+        for i in range(3):
+            axes_12[i].remove()   
+            axes_12[i] = ax.quiver(float(row[11]), float(row[12]), float(row[13]), q[0][i], q[1][i], q[2][i], length=0.1, normalize=True, color=colors[i])
 
-ani = FuncAnimation(fig, update, frames=range(1500, len(rows)), interval=10, repeat=False)
+ani = FuncAnimation(fig, update, frames=range(0, len(rows)), interval=10, repeat=False)
 plt.show()
